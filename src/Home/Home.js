ae325735ac91819e569ef95c0eb2c00ff0827a88
Dávid Móczár <moczardavid@gmail.com>
2018-10-29 20:52:37 +0100

    Added socket.io

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,25 @@
 import { Component } from 'react';
-
+import io from 'socket.io-client';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.socket = io('http://localhost:3001/', { query: `token=${localStorage.getItem('id_token')}` });
+    this.setupSocket();
+    this.state = {
+      hello: 'new hello',
+    };
+  }
+
+  setupSocket() {
+    this.socket.on('hello', () => {
+      this.setState({ hello: 'hello' });
+    });
+  }
+
   render() {
     return (
-      'asd'
+      `message: ${this.state.hello}`
     );
   }
 }
